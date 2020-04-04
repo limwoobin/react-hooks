3f25291223c889fe8efd945e11577d1bc1f79c7f
drogba <drogba02@naver.com>
2020-04-04 18:40:04 +0900

    테스트 js 생성

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Home from './Home';
 import DynamicPage from './DynamicPage';
 import NoMatch from './NoMatch';
 import Header from './layout/Header/Header';
+import test from './test';
+
 
 const App = () => {
   return (
@@ -12,7 +14,8 @@ const App = () => {
         <div style={{paddingTop: '75px' , minHeight: 'calc(100vh - 80px)' }}>
           <BrowserRouter>
             <Switch>
-              <Route exact path="/" component={Home} />
+              {/* <Route exact path="/" component={Home} /> */}
+              <Route exact path="/" component={test} />
               <Route exact path="/dynamic" component={DynamicPage} />
               <Route component={NoMatch} />
             </Switch>
