4f959755a94c33513913da602e29fbdac2047fbf
drogba <drogba02@naver.com>
2020-04-13 21:05:33 +0900

    test Counter 추가

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,7 @@ import React , {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  const [data , setData] = useState({
-    count: 0,
-  });
+  const [count , setCount] = useState(0);
   const [text , setText] = useState('');
   
   const updateField = (e) => {
@@ -14,6 +12,14 @@ const Home = () => {
     console.log(nextState[e.target.name]);
   }
 
+  const onIncrease = () => {
+    setCount(prevCount => prevCount + 1);
+  }
+
+  const onDecrease = () => {
+    setCount(prevCount => prevCount - 1);
+  }
+
   return (
     <div>
       <p>안녕하세요 리액트를 위한 웹팩.</p>
@@ -21,6 +27,10 @@ const Home = () => {
         <input name="username" />
         <input name="text" value={text} onChange={updateField} />
         <Link to="/hooks">Hooks Input로 이동</Link>
+
+        <h1>{count}</h1>
+        <button onClick={onIncrease}>+</button>
+        <button onClick={onDecrease}>-</button>
       </p>
     </div>
   );
