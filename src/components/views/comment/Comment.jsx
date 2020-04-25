ce91fad590df46d55d73c4612681c59f0b8bf29f
drogba <drogba02@naver.com>
2020-04-25 21:02:48 +0900

    댓글 컴포넌트 추가

--- src/components/views/comment/Comment.jsx
+++ src/components/views/comment/Comment.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import Divider from '@material-ui/core/Divider';
+import ChildComment from './ChildComment';
+import { Func } from '../../../common/common';
 
-const Comment = () => {
+const Comment = (props) => {
+    const {userEmail , content , modiDate , childComments} = props;
+    const renderChildComments = (data) => {
+        console.log('data:' + data);
+        return data.map((c) => {
+            return <ChildComment 
+                        key={c._id}
+                        image={c.image}
+                        childCommentId={c.childCommentId}
+                        userEmail={c.userEmail}
+                        content={c.content}
+                        modiDate={c.modiDate}
+                    />
+        });
+    }
+
     return (
         <div>
-            
+            {userEmail} : {content} &nbsp; {Func.DateFormat(modiDate)} <br/>
+            {childComments ? renderChildComments(childComments)
+            : ''}
+            &nbsp;
+            <Divider />
         </div>
     )
 }
